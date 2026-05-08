afc8359adf5d26cb2c075524a7b1a38b9cba5e80
xuxin1955 <x2874836915@qq.com>
2026-05-09 05:42:16 +0800

    update

--- diy-part7.sh
+++ diy-part7.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+
 # SKW97
 mkdir -p target/linux/ath79/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/qca9531_compex_wpj531-16m.dts" "target/linux/ath79/dts/qca9531_compex_wpj531-16m.dts"
