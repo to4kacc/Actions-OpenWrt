1d8ce79cff3e3a4bf6969bcb368213deee2f5f2e
xuxin1955 <x2874836915@qq.com>
2026-06-14 09:39:19 +0800

    update

--- diy-part7.sh
+++ diy-part7.sh
@@ -9,8 +9,8 @@ mkdir -p target/linux/ramips/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7620a_glinet_gl-mt750.dts" "target/linux/ramips/dts/mt7620a_glinet_gl-mt750.dts"
 
 mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
-cp -f "$GITHUB_WORKSPACE/scripts/dts/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
+cp -f "$GITHUB_WORKSPACE/scripts/mt7620/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
 
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
