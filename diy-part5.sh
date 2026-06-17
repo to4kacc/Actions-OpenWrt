51ce9123a774b694f684b89abb148f6f6a6271d9
xuxin1955 <x2874836915@qq.com>
2026-06-17 17:15:20 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -4,7 +4,8 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # turboacc
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # hlk7620A dts
 mkdir -p target/linux/ramips/dts/
