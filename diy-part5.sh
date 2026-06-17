d78b008a2e61d8a79851562debfc113b8baf1e6f
xuxin1955 <x2874836915@qq.com>
2026-06-17 17:01:17 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -3,9 +3,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
+# turboacc
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 # hlk7620A dts
 mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_zbtlink_zbt-we826-32m.dts" "target/linux/ramips/dts/mt7620a_zbtlink_zbt-we826-32m.dts"
 cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_glinet_gl-mt750.dts" "target/linux/ramips/dts/mt7620a_glinet_gl-mt750.dts"
 
 mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
